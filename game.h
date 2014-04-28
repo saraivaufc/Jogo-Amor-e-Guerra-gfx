4ad13a34ef9f91d0397838d113ab922071baae7b
Ciano <cianohanter@hotmail.com>
2014-04-27 21:08:25 -0300

    Create game.h

---
+++ game.h
@@ -0,0 +1,31 @@
+void Iniciar_Game();//essa funcão cria a janela inicial
+
+void Iniciar_Variaveis();//essa função inicializa todas as variaveis do jogo
+
+void Recebe_Comando();//recebe um comando do teclado
+
+void Proxima_Posicao_Robo(); //essa funcão calcula qual será a proxima posição do Robo
+
+void Proxima_Posicao_Coracao();//essa função calcula a posição de todos os corações
+
+void Proxima_Posicao_Projetil(); //verifica qual será a proxima posição do Projetil se esse "Existir();"
+
+void Trata_Posicao_Robo(); //impede que o robo se choque o a parede
+
+void Trata_Posicao_Coracao(); //impede que algum coração se choque com a parede e com o Robo
+
+void Trata_Posicao_Projetil();//verifica se o projetiu se chocou com algum Coração ou com alguma parede
+
+void Atualizar(); //essa função atualiza o estado do Jogo
+
+void Verifica_Vitoria(); //verifica se o jogador ganhou o jogo
+
+void Desenha_Robo(); //Imprime o Robo
+
+void Desenha_Coracao(); //Imprime todos os corações
+
+void Desenha_Projetil(); //Se Projetil existir(); imprime ele
+
+void Zerar_Variaveis(); //Zera algumas variaveis que precisam ser zeradas após cada execução
+
+
